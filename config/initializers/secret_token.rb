40480762b4b71ca0c9b54476a6e7dc1f383fa39b
Miclovich <victor.miclovich@storyspaces.org>
2011-03-04 11:42:47 +0300

    first one

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EpromMit::Application.config.secret_token = 'e5be40efa6ca7e766dd4129c945869613e2f3c83ba9a0dc389bf2928b5606d019d54ea9863f057f0ded84251c99cb5d36b872522877209af3a86e18d33b3301f'
